981519876e9721719551bd4c20bf618b59b1b64d
minseok <myminseok@gmail.com>
2021-12-19 18:38:39 +0000

    nginx

--- samples/nginx/tls-cert-manager/fetch-nginx-tls.sh
+++ samples/nginx/tls-cert-manager/fetch-nginx-tls.sh
@@ -1,2 +1,2 @@
-NS=nginx-test
+NS=default
 kubectl get secret nginx-tls -o yaml -o jsonpath='{.data.tls\.crt}' -n $NS | base64 -d > nginx-tls.crt
--- samples/nginx/tls-cert-manager/generate-nginx-tls.sh
+++ samples/nginx/tls-cert-manager/generate-nginx-tls.sh
@@ -1,4 +1,4 @@
-NS=nginx-test
+NS=default
 kubectl create ns $NS
 kubectl apply -f self-signed-issuer.yml -n $NS
 kubectl apply -f self-signed-certs-lab-pcfdemo.net.yml -n $NS
