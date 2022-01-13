99d081df0dab1c2b16597027f6431dccc2a4e94c
Your Name <you@example.com>
2022-01-13 03:54:47 +0000

    adding ako 1.5.1

--- ako/install_ako.sh
+++
@@ -1,6 +0,0 @@
-#helm repo add ako https://projects.registry.vmware.com/chartrepo/ako
-# helm show values ako/ako --version 1.4.2 > values.yaml
-
-kubectl create ns avi-system
-# projects.registry.vmware.com/ako/ako:1.4.2
-helm install ako/ako --generate-name --version 1.4.2 -f values.yaml --set ControllerSettings.controllerIP=avi.lab.pcfdemo.net --set avicredentials.username=admin --set avicredentials.password=VMware123!@# --namespace=avi-system
