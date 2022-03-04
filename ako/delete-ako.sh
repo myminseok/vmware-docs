072a6554627127d43d94304298a7d083026a4ee3
Your Name <you@example.com>
2022-03-04 15:04:43 +0000

    ako

--- ako/delete-ako.sh
+++ ako/delete-ako.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+
 helm list -n avi-system
-# helm uninstall -n avi-system ako-1639937593h
+target=$(helm list -n avi-system -o yaml | grep "name:" | cut -d ":" -f 2)
+echo $target
+helm uninstall -n avi-system $target
