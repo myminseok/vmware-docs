2992f27546d413f38e3df7d2d693f3c97e238389
myminseok <myminseok@gmail.com>
2022-11-18 18:44:16 +0900

    yelb

---
+++ samples/yelb-main/docker-images/convert-image-url.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -ex
+
+## required: define source file folder to work
+SOURCE_ABS=$( cd ../yelb/  && pwd )
+
+TARGET_FOLDER="${SOURCE_ABS}-converted"
+TARGET_URL="harbor.h2o-2-2257.h2o.vmware.com\/library"
+rm -rf $TARGET_FOLDER
+cp -r $SOURCE_ABS $TARGET_FOLDER
+
+## detect remote os.
+OS=$(uname)
+
+
+
+while read -r image_url; do
+  image=$(echo $image_url | rev| cut -d'/' -f1 | rev )
+  image_url_encoded=$(echo $image_url | sed  's/\//\\\//g'  )
+  if [ "$OS" == "Darwin" ]; then
+    command="find $TARGET_FOLDER -maxdepth 1 -type f  | xargs  sed -i '' 's/$image_url_encoded/$TARGET_URL\/$image/g' "
+  else
+    command="find $TARGET_FOLDER -maxdepth 1 -type f  | xargs  sed -i    's/$image_url_encoded/$TARGET_URL\/$image/g' "
+  fi
+  eval $command 
+done < docker-images.txt
